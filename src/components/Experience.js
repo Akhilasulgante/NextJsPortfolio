624cad93b9491b0ba9df8862ef0ba79d2d017646
Akhila Sulgante <asulgante39@gmail.com>
2023-11-30 11:20:49 -0800

    Changed skills viz from collapsible tree to space tree

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -42,7 +42,7 @@ const Experience = () => {
   });
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-16 w-full text-center md:text-6xl xs:text-4xl md:mt-16">
+      <h2 className="font-bold text-8xl pt-2 mb-16 w-full text-center md:text-6xl xs:text-4xl md:mt-16">
         Experience
       </h2>
       <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,15 +4,21 @@ import ObservableNotebook from "./ObservableNotebook";
 const skills = () => {
   return (
     <div>
-      <h2 className="font-bold text-8xl mt-32 mb-16 w-full text-center md:text-6xl md:mt-32 ">
+      <h2 className="font-bold text-8xl mt-32 w-full text-center md:text-6xl md:mt-32 ">
         Skills
       </h2>
       {/* <div className="w-full"> */}
-      <iframe
+      {/* <iframe
         width="100%"
         height="424"
         frameborder="0"
         src="https://observablehq.com/embed/2f0505f1863422ba?cells=chart"
+      ></iframe> */}
+      <iframe
+        width="100%"
+        height="700"
+        frameborder="0"
+        src="https://observablehq.com/embed/0d85e33652e41561?cells=viewof+animate%2Cviewof+selection"
       ></iframe>
       {/* </div> */}
     </div>
