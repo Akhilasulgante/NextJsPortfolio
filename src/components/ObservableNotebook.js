c11a2f482581d63ba443a82d41c0ef9a4adbc294
Akhila Sulgante <asulgante39@gmail.com>
2023-11-29 17:05:38 -0800

    Changed observable from react fragment to iframe

--- src/components/ObservableNotebook.js
+++ src/components/ObservableNotebook.js
@@ -3,24 +3,24 @@ import { Runtime, Inspector } from "@observablehq/runtime";
 import notebook from "bd919d45bc83756d";
 
 function Notebook() {
-  const chartRef = useRef();
+  // const chartRef = useRef();
 
-  useEffect(() => {
-    const runtime = new Runtime();
-    runtime.module(notebook, (name) => {
-      if (name === "chart") return new Inspector(chartRef.current);
-    });
-    return () => runtime.dispose();
-  }, []);
+  // useEffect(() => {
+  //   const runtime = new Runtime();
+  //   runtime.module(notebook, (name) => {
+  //     if (name === "chart") return new Inspector(chartRef.current);
+  //   });
+  //   return () => runtime.dispose();
+  // }, []);
 
   return (
     <>
-      <div ref={chartRef} />
-      <p>
-        <a href="https://observablehq.com/d/bd919d45bc83756d@194">
-          Zoomable circle packing by Akhila Sulgante
-        </a>
-      </p>
+      <iframe
+        width="100%"
+        height="422"
+        frameborder="0"
+        src="https://observablehq.com/embed/bd919d45bc83756d?cells=chart"
+      ></iframe>
     </>
   );
 }
